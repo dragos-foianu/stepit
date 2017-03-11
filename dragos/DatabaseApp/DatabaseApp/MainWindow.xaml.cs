a813b385a545403c45c2c0a58f176ff8de4a4639
Dragos Foianu <dragos.foianu@gmail.com>
2017-03-11 10:58:53 +0200

    Add/Remove in C#

--- dragos/DatabaseApp/DatabaseApp/MainWindow.xaml.cs
+++ dragos/DatabaseApp/DatabaseApp/MainWindow.xaml.cs
@@ -26,31 +26,93 @@ namespace DatabaseApp
     {
         DataTable MyDataTable { get; set; }
 
+        private MySqlConnection conn;
+
         public MainWindow()
         {
             InitializeComponent();
             MyDataTable = new DataTable();
 
             string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
-            MySqlConnection conn = new MySqlConnection(connString);
+            conn = new MySqlConnection(connString);
+            conn.Open();
 
-            string sqlCommand = "select * from studenti";
+            string sqlCommand = "select * from studenti;";
             try
             {
-                conn.Open();
-
                 MySqlDataAdapter sda = new MySqlDataAdapter(sqlCommand, conn);
                 sda.Fill(MyDataTable);
 
                 MyGuiTable.DataContext = MyDataTable;
                 MyGuiTable.ItemsSource = MyDataTable.DefaultView;
-
-                MessageBox.Show("Sql Command Executed!");
             }
             catch (Exception e)
             {
                 MessageBox.Show("Connection Error! Data: " + e.Data);
             }
+        }
+
+        private void addButton(object sender, RoutedEventArgs e)
+        {
+            string id = IdBox.Text;
+            string nume = NumeBox.Text;
+            string nota = NotaBox.Text;
+            string comment = CommentBox.Text;
+
+            string sqlCommand = "select * from studenti;";
+            string insertCommand = String.Format("insert into studenti values ('{0}', '{1}', '{2}', '{3}');", id, nume, nota, comment);
+
+            try
+            {
+                MyDataTable.Clear();
+                MySqlCommand ins = new MySqlCommand(insertCommand, conn);
+                MySqlDataAdapter sda = new MySqlDataAdapter(sqlCommand, conn);
+                ins.ExecuteNonQuery();
+                sda.Fill(MyDataTable);
+
+                MyGuiTable.DataContext = MyDataTable;
+                MyGuiTable.ItemsSource = MyDataTable.DefaultView;
+
+                MessageBox.Show("Add Command Executed!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connection Error!");
+            }
+        }
+
+        private void removeButton(object sender, RoutedEventArgs e)
+        {
+            string id = IdBox.Text;
+            string nume = NumeBox.Text;
+            string nota = NotaBox.Text;
+            string comment = CommentBox.Text;
+
+            string sqlCommand = "select * from studenti;";
+            string deleteCommand = String.Format("delete from studenti where ID='{0}' or Name='{1}' or Nota='{2}' or Comment='{3}';", id, nume, nota, comment);
+
+            try
+            {
+                MyDataTable.Clear();
+                MySqlCommand ins = new MySqlCommand(deleteCommand, conn);
+                MySqlDataAdapter sda = new MySqlDataAdapter(sqlCommand, conn);
+                ins.ExecuteNonQuery();
+                sda.Fill(MyDataTable);
+
+                MyGuiTable.DataContext = MyDataTable;
+                MyGuiTable.ItemsSource = MyDataTable.DefaultView;
+
+                MessageBox.Show("Remove Command Executed!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connection Error!");
+            }
+        }
+
+        private void updateButton(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
